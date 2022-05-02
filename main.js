e4f9147c8a96e3c080c1f14ad53d3c9bf2ee1be4
ghostfxck <47380928+ghostfxck@users.noreply.github.com>
2022-05-03 00:02:37 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+$(function () {
+  $("#btn").click(function () {
+    var cock = $("#cock").val();
+    var avatar = $('#avatar').val();
+    var username = $("#username").val();
+    var content = $("#content").val();
+    if ((cock == null || cock == "", content == null || content == "")) {
+      alert("Please Fill Out All The Fields");
+      return false;
+    }
+    $.post(cock, { content: content, username: username, "avatar_url": avatar, });
+  });
+});
